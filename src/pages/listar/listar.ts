125506308e598d861476ed413108b1ce3e5f438b
Jhonata Ribeiro <jhonata.ribeiro.ti@gmail.com>
2018-09-19 16:52:41 -0300

    criado searchbar

--- src/pages/listar/listar.ts
+++ src/pages/listar/listar.ts
@@ -17,34 +17,60 @@ import { DbAgendaProvider } from '../../providers/db-agenda/db-agenda';
 })
 export class ListarPage {
 
-  public listaContatos:Array<any> = [];
+  public listaContatos: Array<any> = [];
 
   constructor(
-    public navCtrl: NavController, 
+    public navCtrl: NavController,
     public navParams: NavParams,
-    public dbAgendaProvider:DbAgendaProvider) {
-      this.listarConatatos();
+    public dbAgendaProvider: DbAgendaProvider) {
+    this.listarConatatos();
   }
 
-  listarConatatos(){
+  listarConatatos() {
     this.dbAgendaProvider.listar().subscribe(
       data => {
         this.listaContatos = [] = data.rows;
-        console.log(this.listaContatos);
-      },err=>{
+        console.log("chamou listar contatos: ", this.listaContatos);
+      }, err => {
         console.log("error", err)
       }
     )
   }
 
-  deletarConatato(contato){
+  deletarConatato(contato) {
     this.dbAgendaProvider.removeContato(contato).subscribe(
       data => {
         this.listarConatatos();
-      },err=>{
+      }, err => {
         console.log("error", err)
       }
     )
+  }
+
+  getItems(searchbar) {
+    // Reset items back to all of the items
+
+    // set q to the value of the searchbar
+    var q = searchbar.srcElement.value;
+
+
+    // if the value is an empty string don't filter the items
+    if (!q) {
+      this.listarConatatos();
+      return;
+    }
+
+    this.listaContatos = this.listaContatos.filter((v) => {
+      if (v.doc.nome && q) {
+        if (v.doc.nome.toLowerCase().indexOf(q.toLowerCase()) > -1) {
+          return true;
+        }
+        return false;
+      }
+    });
+
+    console.log(q, this.listaContatos.length);
+
   }
 
   ionViewDidLoad() {
